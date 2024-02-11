dc94a4d161b7a64465dd0fd8249f71447be4da79
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2024-02-10 23:11:38 -0300

    Feat: Creating get-poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,10 +2,12 @@ import fastify from "fastify";
 import { prisma } from "../lib/prima";
 import { z } from "zod";
 import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
 
 const app = fastify();
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({port: 3333}).then(() => {
   console.log('HTTP server running!')
